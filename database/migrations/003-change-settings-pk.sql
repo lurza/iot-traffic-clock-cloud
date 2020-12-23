50b26092c14df2003a75255bb37a11f05f9ca5c7
Brecht Vermeersch <brechtvermeersch@outlook.be>
2020-12-23 15:48:05 +0100

    change settings table pk

---
+++ database/migrations/003-change-settings-pk.sql
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+-- Up
+--------------------------------------------------------------------------------
+
+
+CREATE TABLE settings_copy( 
+    key TEXT NOT NULL PRIMARY KEY,
+    value TEXT NOT NULL
+);
+
+INSERT INTO settings_copy(key, value)
+SELECT key, value FROM settings;
+
+DROP TABLE settings;
+ALTER TABLE settings_copy RENAME TO settings;
